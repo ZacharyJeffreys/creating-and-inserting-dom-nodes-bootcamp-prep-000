625324794c005fe2ce41d041ed80d9665225d8ba
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-29 02:55:00 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 var element = document.createElement('div');
 element.innerHTML = 'Im a savage!';
 element.style.backgroundColor = '#f9f9f9';
+element.style.textAlign = `center`;
 document.body.appendChild(element);
