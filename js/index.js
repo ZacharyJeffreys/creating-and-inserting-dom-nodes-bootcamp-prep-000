70f0e44ae5f6703bc7ca2352d08830543f237039
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-29 02:55:43 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,3 +3,13 @@ element.innerHTML = 'Im a savage!';
 element.style.backgroundColor = '#f9f9f9';
 element.style.textAlign = `center`;
 document.body.appendChild(element);
+
+var ul = document.createElement('ul');
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+element.appendChild(ul);
